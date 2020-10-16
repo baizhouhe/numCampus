4d613c0d555066f880f5acedfd1b18b066b6be7b
chenyunpeng <chyunp@aliyun.com>
2020-10-16 13:36:04 +0800

    add 1

---
+++ public-project/src/main/java/com/xty/campus/mapper/mapper/DepartmentMapper.java
@@ -0,0 +1,7 @@
+package com.xty.campus.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xty.campus.baseTable.Department;
+
+public interface DepartmentMapper extends BaseMapper<Department> {
+}
